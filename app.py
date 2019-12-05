ba1838a65335bcbc7b41f81041f25a844dd34eba
Ravi Dahiya <ravidahiya74@gmail.com>
2019-12-05 17:01:47 +0000

    added app

---
+++ app.py
@@ -0,0 +1,38 @@
+import numpy as np
+from flask import Flask, request,render_template
+from flask_cors import CORS
+import os
+from sklearn.externals import joblib
+import pickle
+import flask
+import os
+app = Flask(__name__)
+CORS(app)
+
+app=flask.Flask(__name__,template_folder='templates')
+
+filename = 'finalized_model.sav'
+model = joblib.load(filename)
+with open('model.pickle', 'rb') as handle:
+	model = pickle.load(handle)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    temp=request.get_data(as_text=True)
+    new=[temp]
+    message=pipeline.transform(new)
+    pred = pipeline.predict(message)
+    if pred == 1:
+        return "SPAM"
+    else:
+        return "HAM"
+    return str(pred)
+    
+if __name__=="__main__":
+    port=int(os.environ.get('PORT',5000))
+    app.run(port=port,debug=True,use_reloader=False)
+    
